453d23668f2fbfc237ce47eaa9c12af19734104c
Rahul <rahul.pandita41299@gmail.com>
2022-07-12 14:46:32 +0530

    random hex value generator

--- 3-color-flipper/js/hex.js
+++ 3-color-flipper/js/hex.js
@@ -6,8 +6,13 @@ const color = document.querySelector(".color");
 button.addEventListener("click", function () {
     let hexColor = "#";
     for (let i = 0; i < 6; i++) {
-        hexColor += hex[Math.floor(Math.random()*hex.length)] // RANDOM HEX NUMBER
+        hexColor += hex[Math.floor(Math.random() * hex.length)]; // RANDOM HEX NUMBER
+        // hexColor += hex[getRandomNumber];
     }
     color.textContent = hexColor;
     document.body.style.backgroundColor = hexColor;
-})
+});
+
+// function getRandomNumber() {
+//     return Math.floor(Math.random() * hex.length);
+// }
